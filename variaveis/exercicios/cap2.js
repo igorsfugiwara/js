fca59d9923a2f76c6a4dd6086da84af17aa4215c
Igor Fugiwara <ifugiwara11@uolinc.com>
2023-05-15 19:21:33 -0300

    estudos

--- variaveis/exercicios/cap2.js
+++ variaveis/exercicios/cap2.js
@@ -227,3 +227,30 @@ var cachorro = {
     }
 }
 cachorro.latir('homem');
+
+
+// --------AULA 208
+
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+let brasilCampeao = [1959, 1962, 1970, 1994, 2002];
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+    brasilCampeao.forEach( ano => {
+        console.log(`O brasil ganhou a copa de ${ano}`);
+    });
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+
+frutas.forEach( element => {
+    if(element === 'Pera'){
+        return console.log('array iterada até "Pera"');
+    }
+});
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+    let ultimaFruta = frutas.slice(-1).toString();
+    console.log(ultimaFruta);
